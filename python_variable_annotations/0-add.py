5c9d3ede9e24906afa708bb7b3353771bfc8b084
N4yth <nathan.dupuis31@gmail.com>
2025-07-28 08:57:40 +0200

    styles : change space

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
-
 """
 module with 1 function
 """
+
+
 def add(a: float, b: float) -> float:
     """
     function use to concatenate 2 float
--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
-
 """
 module with 1 function
 """
+
+
 def concat(str1: str, str2: str) -> str:
     """
     function use to concatenate 2 string
