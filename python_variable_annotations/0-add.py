b530760e4db72998eac27e95e671b335b140c152
N4yth <nathan.dupuis31@gmail.com>
2025-07-28 08:55:59 +0200

    doc : add documentation to function and module

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
 
+"""
+module with 1 function
+"""
+
 def add(a: float, b: float) -> float:
+    """
+    function use to concatenate 2 float
+    """
     return (a+b)
--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
 
+"""
+module with 1 function
+"""
+
 def concat(str1: str, str2: str) -> str:
+    """
+    function use to concatenate 2 string
+    """
     return (str1+str2)
