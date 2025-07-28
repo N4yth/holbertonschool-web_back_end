25500f08302baf92682dc8692594318488aaaa4e
N4yth <nathan.dupuis31@gmail.com>
2025-07-28 13:41:32 +0200

    fix :

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -5,12 +5,13 @@ module with 1 function
 from typing import List
 import asyncio
 
-async def wait_n( n: int, max_delay: int = 10) -> float:
-    wait_random = __import__('0-basic_async_syntax').wait_random
+
+async def wait_n(n: int, max_delay: int = 10) -> List[float]:
     """
     wait for a random time n time
     """
-    result : List= []
+    wait_random = __import__('0-basic_async_syntax').wait_random
+    result: List[float] = []
     for i in range(n):
         result.append(await wait_random(max_delay))
     result.sort()
