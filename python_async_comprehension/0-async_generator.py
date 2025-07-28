fd6837488be3ee5e67fd761eaf95baf1b7d58cb8
N4yth <nathan.dupuis31@gmail.com>
2025-07-28 16:04:04 +0200

    feat : precise return type data

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -4,9 +4,10 @@ module with 1 function
 """
 import asyncio
 import random
+from typing import AsyncGenerator
 
 
-async def async_generator():
+async def async_generator() -> AsyncGenerator[float, None]:
     """
     wait for a random time
     """
