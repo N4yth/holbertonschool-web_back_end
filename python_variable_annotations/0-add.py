45d6145cd29052438c5b36da845bb406fb8f0fac
N4yth <nathan.dupuis31@gmail.com>
2025-07-28 08:56:47 +0200

    styles : remove useless space

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -3,7 +3,6 @@
 """
 module with 1 function
 """
-
 def add(a: float, b: float) -> float:
     """
     function use to concatenate 2 float
--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -3,7 +3,6 @@
 """
 module with 1 function
 """
-
 def concat(str1: str, str2: str) -> str:
     """
     function use to concatenate 2 string
