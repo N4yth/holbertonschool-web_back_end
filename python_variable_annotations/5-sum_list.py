f2423a0dea1325311f32f71e3133f556fb9aa925
N4yth <nathan.dupuis31@gmail.com>
2025-07-28 09:51:35 +0200

    doc : remove to much doc

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -2,20 +2,13 @@
 from typing import List
 
 """
-This module provides a utility function for summing
-the elements of a list of floats.
+This module provides a utility function for summing elements
 """
 
 
 def sum_list(input_list: List[float]) -> float:
     """
     Calculate the sum of all elements in a list of floats.
-
-    Args:
-        input_list (List[float]): A list of float values.
-
-    Returns:
-        float: The sum of all values in the input list.
     """
     result: float = 0
     for i in range(len(input_list)):
