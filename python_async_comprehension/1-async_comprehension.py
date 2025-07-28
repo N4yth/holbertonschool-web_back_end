7ef16030247da5f81c38e5ef96a5df1437ca61cd
N4yth <nathan.dupuis31@gmail.com>
2025-07-28 16:44:09 +0200

    feat : create a function that collect 10 random number

---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Asynchronous generator that yields random floats between 0 and 10.
+"""
+import asyncio
+import random
+from typing import Generator
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> Generator[float, None, None]:
+    """
+    collect 10 random numbers using an async comprehensing
+    """
+    result = []
+    async for i in async_generator():
+        result.append(i)
+    return result
