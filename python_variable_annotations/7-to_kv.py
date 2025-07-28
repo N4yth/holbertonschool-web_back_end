b4e430efd6a973c8aab42938592b0f135bd2786e
N4yth <nathan.dupuis31@gmail.com>
2025-07-28 09:43:10 +0200

    feat : create a module to return a tuple with data

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+from typing import Tuple, Union
+"""
+This module provides a utility function that returns a tuple
+containing a string and the square of a numeric value.
+"""
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """
+    Create a tuple with a string and the square of a number.
+
+    Args:
+        k (str): The string key.
+        v (int | float): A number to be squared.
+
+    Returns:
+        Tuple[str, float]: A tuple where the first element is the string `k`,
+                           and the second is the square of `v` as a float.
+    """
+    result : Tuple[str, float] = (k , v**2)
+    return (result)
