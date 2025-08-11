4bb5fa09a0eaefd377655492dc0e52719445b373
N4yth <nathan.dupuis31@gmail.com>
2025-08-11 19:11:11 +0200

    feat : create the python script that will find all matching topic of the given topic

---
+++ NoSQL/11-schools_by_topic.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+module with 1 function
+"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    function that list all document
+    """
+
+    return mongo_collection.find({"topic": topic})
