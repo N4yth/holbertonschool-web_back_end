e3495e9d2c380c8ddfcacc0d12392af566ef4d92
N4yth <nathan.dupuis31@gmail.com>
2025-08-11 19:05:36 +0200

    feat : create a python script that modify topics of a school in the mongodb

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+module with 1 function
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    function that list all document
+    """
+    myquery = {"name": name}
+    newvalues = {"$set": {"topics": topics}}
+
+    mongo_collection.update_one(myquery, newvalues)
