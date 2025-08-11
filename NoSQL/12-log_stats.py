7f3b7ca579b32a0e7c021a13ca6e36b1e01f9af8
N4yth <nathan.dupuis31@gmail.com>
2025-08-11 19:29:54 +0200

    feat : create a python function that display stat of Nginx logs

---
+++ NoSQL/12-log_stats.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+module that print stats of Nginx logs
+"""
+from pymongo import MongoClient
+
+
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    db = client.logs
+    collection = db.nginx
+
+    all_logs = collection.count_documents({})
+    print("{} logs".format(all_logs))
+    print("Methods:")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        count = collection.count_documents({"method": method})
+        print("method {}: {}".format(method, count))
+    status_count = collection.count_documents({
+        "method": "GET", "path": "/status"})
+    print("{} status check".format(status_count))
