82e3fda568469bdd3942a8b61de2c359dd1c076c
N4yth <nathan.dupuis31@gmail.com>
2025-08-11 18:48:10 +0200

    feat : create a python script that add 1 document to the mongodb

---
+++ NoSQL/9-insert_school.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+module with 1 function
+"""
+
+from pymongo import MongoClient
+
+
+def insert_school(mongo_collection, **kwargs):
+    """
+    function that list all document
+    """
+    result = {}
+    for keys, value in kwargs.items():
+        result[keys] = value
+    val = mongo_collection.insert({result})
+    return val.inserted_id
