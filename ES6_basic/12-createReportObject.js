d1cde2eb6fba35d845a616d452ac1d1c30634e4b
N4yth <nathan.dupuis31@gmail.com>
2025-08-18 16:14:23 +0200

    idk

--- ES6_basic/12-createReportObject.js
+++ ES6_basic/12-createReportObject.js
@@ -1,7 +1,5 @@
 export default function createReportObject(employeesList) {
   return {allEmployees: {...employeesList},
-    getNumberOfDepartments(employeesList) {
-      return Object.keys(employeesList).length;
-    },
+    getNumberOfDepartments: () => Object.keys(employeesList).length,
   };
 }
