b70486c112b2c359966f846d3a044295b9057282
N4yth <nathan.dupuis31@gmail.com>
2025-08-18 19:45:11 +0200

    fix

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,9 +1,12 @@
+/* eslint-disable */
 import ClassRoom from "./0-classroom.js";
 
 export default function initializeRooms() {
-  return [
+  const result = [
     new ClassRoom(19),
     new ClassRoom(20),
     new ClassRoom(34),
   ];
+
+  return result;
 }
