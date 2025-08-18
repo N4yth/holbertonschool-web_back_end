37d1391a04e1a7e45c10d7176eb06ff6ca9e7940
N4yth <nathan.dupuis31@gmail.com>
2025-08-18 19:37:50 +0200

    fix : idk

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,9 +1,9 @@
-import ClassRoom from "./0-classroom";
+import ClassRoom from "./0-classroom.fr";
 
-export default function initializeRooms () {
+export default function initializeRooms() {
   return [
     new ClassRoom(19),
     new ClassRoom(20),
     new ClassRoom(34),
-  ]
+  ];
 }
