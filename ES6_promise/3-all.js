e9c82b48a53277c6ce0141696cbef6fe8d48605b
N4yth <nathan.dupuis31@gmail.com>
2025-08-25 15:33:37 +0200

    feat : create a function that collectively resolve all promises

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,10 @@
+import {uploadPhoto} from "./utils.js";
+import {createUser} from "./utils.js";
+
+export default async function handleProfileSignup() {
+  Promise.all([createUser(), uploadPhoto()])
+    .then((value) => {
+      console.log(`${value[1]['body']} ${value[0]['firstName']} ${value[0]['lastName']}`);
+    })
+    .catch(() => new Error('Signup system offline'))
+}
