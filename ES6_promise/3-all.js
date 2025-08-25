157176702928ba657e8cd0e962535b24f1252e20
N4yth <nathan.dupuis31@gmail.com>
2025-08-25 16:00:23 +0200

    fix : change the way to get data

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -4,7 +4,7 @@ import {createUser} from "./utils.js";
 export default function handleProfileSignup() {
   return Promise.all([createUser(), uploadPhoto()])
     .then((value) => {
-      console.log(`${value[1]['body']} ${value[0]['firstName']} ${value[0]['lastName']}`);
+      console.log(`${value[1].body} ${value[0].firstName} ${value[0].lastName}`);
     })
     .catch(() => new Error('Signup system offline'))
 }
