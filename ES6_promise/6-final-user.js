3195eaab2c258513cc44d68a53948e5f9ceb9a48
N4yth <nathan.dupuis31@gmail.com>
2025-08-25 16:31:28 +0200

    fix : return the result

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,6 +2,6 @@ import uploadPhoto from './5-photo-reject.js';
 import signUpUser from "./4-user-promise.js";
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
-  await Promise.allSettled([uploadPhoto(fileName), signUpUser(firstName, lastName)])
+  return await Promise.allSettled([uploadPhoto(fileName), signUpUser(firstName, lastName)])
   .then(results => {return results});
 }
