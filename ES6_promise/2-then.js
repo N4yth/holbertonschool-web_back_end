a244070a6b124b2743d7d28cf6c57f728728eb89
N4yth <nathan.dupuis31@gmail.com>
2025-08-25 14:34:21 +0200

    feat : change the promise use and dirrectly use it

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,15 +1,11 @@
 export default function handleResponseFromAPI(promise) {
-  const prom = new Promise((resolve, reject) => {
-    if (promise) {
-      resolve({
-        'status': 200,
-        'body': 'Success',
-      });
-    }
-    else {
-      reject(new Error());
-    }
-    console.log('Got a response from the API');
-  });
-  return prom;
+  return promise
+    .then(() => ({
+      'status': 200,
+      'body': 'Success',
+    }))
+    .catch(() => new Error())
+    .finally(() => {
+      console.log('Got a response from the API');
+    });
 }
