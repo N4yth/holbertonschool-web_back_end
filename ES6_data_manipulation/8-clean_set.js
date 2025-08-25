8b97819b7fe52f087aaba5536cfaea2da1145ef9
N4yth <nathan.dupuis31@gmail.com>
2025-08-25 13:31:47 +0200

    test : change the test of the string

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,5 @@
 export default function cleanSet(set, startString) {
-  if (!typeof startString === 'string' || startString === '') {
+  if (!startString || startString === '') {
     return '';
   }
   var start = true;
