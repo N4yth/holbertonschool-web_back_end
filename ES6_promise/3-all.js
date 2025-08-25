f093f8166e7a78add79b93e27c34a7fb10653fcb
N4yth <nathan.dupuis31@gmail.com>
2025-08-25 15:45:26 +0200

    feat : add return to the promise

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -2,7 +2,7 @@ import {uploadPhoto} from "./utils.js";
 import {createUser} from "./utils.js";
 
 export default function handleProfileSignup() {
-  Promise.all([createUser(), uploadPhoto()])
+  return Promise.all([createUser(), uploadPhoto()])
     .then((value) => {
       console.log(`${value[1]['body']} ${value[0]['firstName']} ${value[0]['lastName']}`);
     })
