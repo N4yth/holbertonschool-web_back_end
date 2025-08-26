a81c02d8e9d63e7d7980f3e6672bd755a35a57b4
N4yth <nathan.dupuis31@gmail.com>
2025-08-26 10:25:14 +0200

    style : Eslint correct

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,11 +2,11 @@ import uploadPhoto from './5-photo-reject';
 import signUpUser from './4-user-promise';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([uploadPhoto(fileName), signUpUser(firstName, lastName)])
-    .then((results) => 
-      results.map((res) => ({
-        status: res.status,
-        value: res.status === 'fulfilled' ? res.value : String(res.reason),
-      }))
-    );
+  return Promise.allSettled([
+    uploadPhoto(fileName),
+    signUpUser(firstName, lastName),
+  ]).then((results) => results.map((res) => ({
+    status: res.status,
+    value: res.status === 'fulfilled' ? res.value : String(res.reason),
+  })));
 }
