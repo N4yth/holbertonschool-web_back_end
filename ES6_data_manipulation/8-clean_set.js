5fc9579650f9a2de79fa1bdbb8e847f93aa46761
N4yth <nathan.dupuis31@gmail.com>
2025-08-26 13:49:03 +0200

    fix

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,11 @@
 export default function cleanSet(set, startString) {
-  if (!startString || startString === '') {
+  if (!startString || startString.length === 0) {
     return '';
   }
+
   let start = true;
   let text = [];
+  
   for (const value of set) {
     if (typeof value === 'string') {
       if (value.indexOf(startString) === 0) {
