bf313d60b6cde9049819aa27c4a9f869f733fcd2
N4yth <nathan.dupuis31@gmail.com>
2025-09-01 14:28:02 +0200

    fix : handle the potential error of function call

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -4,9 +4,14 @@ const countStudents = require('./3-read_file_async');
 const app = http.createServer((req, res) => {
   if (req.url === '/students') {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    countStudents(process.argv[2]).then(function(result){
-      res.end(`This is the list of our students\n${result}`);
-    }) 
+
+    countStudents(process.argv[2])
+      .then((result) => {
+        res.end(`This is the list of our students\n${result}`);
+      })
+      .catch((err) => {
+        res.end(`This is the list of our students\n${err.message}`);
+      });
   } else {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello Holberton School!');
