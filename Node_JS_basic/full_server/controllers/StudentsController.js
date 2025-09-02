780664d6320409e0149d253beedbb1e44d1b80ee
N4yth <nathan.dupuis31@gmail.com>
2025-09-02 16:04:29 +0200

    fix : use .then instead of try catch

--- Node_JS_basic/full_server/controllers/StudentsController.js
+++ Node_JS_basic/full_server/controllers/StudentsController.js
@@ -1,41 +1,33 @@
 import readDatabase from '../utils';
 
 class StudentsController {
-  static async getAllStudents(request, response, databasePath) {
-    try {
-      let message = 'This is the list of our students';
-      const students = await readDatabase(databasePath);
-      for (const [key] of Object.entries(students).sort()) {
-        message += `\nNumber of students in ${[key]}: ${students[key].length}. List: ${students[key].join(', ')}`;
-      }
-      return {
-        message,
-        status: 200,
-      };
-    } catch (err) {
-      return {
-        message: err.message,
-        status: 500,
-      };
-    }
+  static getAllStudents(request, response, databasePath) {
+
+    let message = 'This is the list of our students';
+    readDatabase(databasePath)
+      .then((students) => {
+        for (const [key] of Object.entries(students).sort()) {
+          message += `\nNumber of students in ${[key]}: ${students[key].length}. List: ${students[key].join(', ')}`;
+        }
+        response.status(200).send(message);
+      })
+      .catch((err) => {
+        response.status(500).send(err.message);
+      });
   }
 
-  static async getAllStudentsByMajor(request, response, databasePath, major) {
-    try {
-      if (major !== 'CS' && major !== 'SWE') {
-        throw new Error('Major parameter must be CS or SWE');
-      }
-      const students = await readDatabase(databasePath);
-      return {
-        message: `List: ${students[major].join(', ')}`,
-        status: 200,
-      };
-    } catch (err) {
-      return {
-        message: err.message,
-        status: 500,
-      };
+  static getAllStudentsByMajor(request, response, databasePath) {
+    const major = request.params.major
+    if (major !== 'CS' && major !== 'SWE') {
+      response.status(500).send('Major parameter must be CS or SWE');
     }
+    readDatabase(databasePath)
+      .then((students) => {
+        response.status(200).send(`List: ${students[major].join(', ')}`);
+      })
+      .catch((err) => {
+        response.status(500).send(err.message);
+      });
   }
 }
 
