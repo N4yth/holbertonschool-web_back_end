1ce8862aadd89c1ca3e841e84b33e5c3e39bf7b8
N4yth <nathan.dupuis31@gmail.com>
2025-09-02 14:28:56 +0200

    fix : ordered result

--- Node_JS_basic/full_server/controllers/StudentsController.js
+++ Node_JS_basic/full_server/controllers/StudentsController.js
@@ -5,9 +5,14 @@ class StudentsController {
     try {
       const students = await readDatabase(databasePath);
       return {
-        message: 'This is the list of our students\n'
-          + `Number of students in CS: ${students.CS.length}. List: ${students.CS.join(', ')}\n`
-          + `Number of students in SWE: ${students.SWE.length}. List: ${students.SWE.join(', ')}`,
+        message:
+          'This is the list of our students\n'
+          + `Number of students in CS: ${students.CS.length}. List: ${students.CS
+            .sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()))
+            .join(', ')}\n`
+          + `Number of students in SWE: ${students.SWE.length}. List: ${students.SWE
+            .sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()))
+            .join(', ')}`,
         status: 200,
       };
     } catch (err) {
