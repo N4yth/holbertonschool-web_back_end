adec45fa5ec83daeb3684ba40f65fede39147c2a
N4yth <nathan.dupuis31@gmail.com>
2025-09-02 13:44:45 +0200

    style : remove the starting message

--- Node_JS_basic/full_server/server.js
+++ Node_JS_basic/full_server/server.js
@@ -1,8 +1,6 @@
 const app = require('express')();
 require('./routes/index')(app, process.argv[2]);
 
-app.listen(1245, () => {
-  console.log('server is running');
-});
+app.listen(1245, () => {});
 
 export default app;
