569c8f0b69c0b69210be7b25061ba727366695d1
N4yth <nathan.dupuis31@gmail.com>
2025-09-02 14:47:46 +0200

    fix : stocke the result of the function of the homepage

--- Node_JS_basic/full_server/routes/index.js
+++ Node_JS_basic/full_server/routes/index.js
@@ -1,9 +1,10 @@
 import AppController from '../controllers/AppController';
 import StudentsController from '../controllers/StudentsController';
 
-module.exports = async (app, databasePath) => {
+module.exports = (app, databasePath) => {
   app.get('/', (req, res) => {
-    res.status(result.status).send(AppController.getHomepage(req, res).message);
+    const result = AppController.getHomepage(req, res);
+    res.status(result.status).send(result.message);
   });
 
   app.get('/students', async (req, res) => {
@@ -15,7 +16,6 @@ module.exports = async (app, databasePath) => {
     const result = await StudentsController.getAllStudentsByMajor(
       req, res, databasePath, req.params.major,
     );
-    console.log(result)
     res.status(result.status).send(result.message);
   });
 };
