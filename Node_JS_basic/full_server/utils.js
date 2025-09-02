72a70e289fd5fd330940be480b71f7c666a37748
N4yth <nathan.dupuis31@gmail.com>
2025-09-02 16:26:36 +0200

    feat : return a promise

--- Node_JS_basic/full_server/utils.js
+++ Node_JS_basic/full_server/utils.js
@@ -1,31 +1,36 @@
-import { promises as fs } from 'fs';
+import fs from 'fs';
 
 function readDatabase(path) {
-  return fs.readFile(path, 'utf8')
-    .then((data) => {
-      let start = true;
-      const nbStudentCS = [];
-      const nbStudentSWE = [];
-      const rows = data.split('\n');
-      rows.forEach((row) => {
-        const columns = row.split(',');
-        if (!start && row !== '') {
-          if (columns[3] === 'CS') {
-            nbStudentCS.push(columns[0]);
-          } else {
-            nbStudentSWE.push(columns[0]);
-          }
-        }
-        start = false;
-      });
-      return {
-        CS: nbStudentCS,
-        SWE: nbStudentSWE,
-      };
-    })
-    .catch(() => {
-      throw new Error('Cannot load the database');
+  const promise = new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+      }
+      resolve(data);
     });
+  });
+
+  return promise.then((data) => {
+    let start = true;
+    const nbStudentCS = [];
+    const nbStudentSWE = [];
+    const rows = data.split('\n');
+    rows.forEach((row) => {
+      const columns = row.split(',');
+      if (!start && row !== '') {
+        if (columns[3] === 'CS') {
+          nbStudentCS.push(columns[0]);
+        } else {
+          nbStudentSWE.push(columns[0]);
+        }
+      }
+      start = false;
+    });
+    return {
+      CS: nbStudentCS,
+      SWE: nbStudentSWE,
+    };
+  });
 }
 
 export default readDatabase;
