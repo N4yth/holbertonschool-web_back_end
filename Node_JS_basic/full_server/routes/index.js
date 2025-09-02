20eca4e1a04a577efaeb386ceb128a7435388762
N4yth <nathan.dupuis31@gmail.com>
2025-09-02 14:44:08 +0200

    fix : send the status in first and the msg in second

--- Node_JS_basic/full_server/routes/index.js
+++ Node_JS_basic/full_server/routes/index.js
@@ -3,18 +3,19 @@ import StudentsController from '../controllers/StudentsController';
 
 module.exports = async (app, databasePath) => {
   app.get('/', (req, res) => {
-    res.send(AppController.getHomepage(req, res).message);
+    res.status(result.status).send(AppController.getHomepage(req, res).message);
   });
 
   app.get('/students', async (req, res) => {
     const result = await StudentsController.getAllStudents(req, res, databasePath);
-    res.send(result.message).status(result.status);
+    res.status(result.status).send(result.message);
   });
 
   app.get('/students/:major', async (req, res) => {
     const result = await StudentsController.getAllStudentsByMajor(
       req, res, databasePath, req.params.major,
     );
-    res.send(result.message).status(result.status);
+    console.log(result)
+    res.status(result.status).send(result.message);
   });
 };
